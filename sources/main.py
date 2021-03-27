f3453b742a4e090c0f4e632cf6e4518aca1c6a7a
Sergey Opryshko <sergeioprishko@gmail.com>
2021-03-27 21:51:17 +0300

    Добавил драйверы, мультипроцессинг. Начал работать над прокси

---
+++ sources/main.py
@@ -0,0 +1,53 @@
+import os, sys
+import zipfile
+import proxies
+from selenium.webdriver import ChromeOptions, Chrome, ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from joblib import Parallel, delayed
+
+product_url = 'https://kith.com/account'
+
+window_size = '700,700'
+chrome_options = ChromeOptions()
+# chrome_options.add_argument('headless')
+chrome_options.add_argument('ignore-certificate-errors')
+chrome_options.add_argument(f'window-size={window_size}')
+chrome_options.add_experimental_option('detach', True)
+
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path).replace('\\', '/')
+
+
+driver_path = resource_path('chromedriver_89')
+
+
+def get_chromedriver(use_proxy=False, user_agent=None):
+    if use_proxy:
+        plugin_file = 'proxy_auth_plugin.zip'
+        with zipfile.ZipFile(plugin_file, 'w') as zp:
+            zp.writestr("manifest.json", proxies.manifest_json)
+            zp.writestr("background.js", proxies.background_js)
+
+        chrome_options.add_extension(plugin_file)
+
+    if user_agent:
+        chrome_options.add_argument('--user-agent=%s' % user_agent)
+
+    driver = Chrome(executable_path=driver_path, chrome_options=chrome_options)
+    return driver
+
+
+def open_drivers():
+    driver = get_chromedriver(use_proxy=False)
+    driver.get(product_url)
+
+
+Parallel(n_jobs=-1)(delayed(open_drivers)() for i in range(1, 3))
---
+++ sources/proxies.py
@@ -0,0 +1,57 @@
+PROXY_HOST = '192.168.3.2'  # rotating proxy or host
+PROXY_PORT = 8080 # port
+PROXY_USER = 'proxy-user' # username
+PROXY_PASS = 'proxy-password' # password
+
+
+manifest_json = """
+{
+    "version": "1.0.0",
+    "manifest_version": 2,
+    "name": "Chrome Proxy",
+    "permissions": [
+        "proxy",
+        "tabs",
+        "unlimitedStorage",
+        "storage",
+        "<all_urls>",
+        "webRequest",
+        "webRequestBlocking"
+    ],
+    "background": {
+        "scripts": ["background.js"]
+    },
+    "minimum_chrome_version":"22.0.0"
+}
+"""
+
+background_js = """
+var config = {
+        mode: "fixed_servers",
+        rules: {
+        singleProxy: {
+            scheme: "http",
+            host: "%s",
+            port: parseInt(%s)
+        },
+        bypassList: ["localhost"]
+        }
+    };
+
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "%s",
+            password: "%s"
+        }
+    };
+}
+
+chrome.webRequest.onAuthRequired.addListener(
+            callbackFn,
+            {urls: ["<all_urls>"]},
+            ['blocking']
+);
+""" % (PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS)
